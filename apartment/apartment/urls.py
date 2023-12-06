ed79f6b91856a2c5652373574f7351a23926dcd3
oohtewy <katanikitin@gmail.com>
2023-12-06 23:12:34 +0500

    media added

--- apartment/apartment/urls.py
+++ apartment/apartment/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls,name='admin'),
     path('', include('market.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
